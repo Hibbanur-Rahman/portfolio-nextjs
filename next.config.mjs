19516a727461fa3f6ae448c5a445aa661eda00fe
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2024-05-27 01:56:58 +0530

    updating the asset prefix

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     output:"export",
     basePath:'/portfolio-nextjs',
-    assetPrefix: 'https://github.com/Hibbanur-Rahman/portfolio-nextjs/',
+    assetPrefix: 'https://github.com/Hibbanur-Rahman/portfolio-nextjs/tree/gh-pages/',
     images:{
         unoptimized:true,
     }
